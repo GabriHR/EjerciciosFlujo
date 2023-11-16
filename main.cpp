b6c9bb7a853127acb5e26cc8bb872aebeb4396b8
KravAD <dkrav@myuax.com>
2023-11-16 12:49:07 +0100

    commit 3
    Funcion main con la implementación del codigo sugerido y llamada de las funciones suma y resta.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,31 @@
 #include <string>
 
 namespace Matematicas {
-} int suma(int a, int b) {
-    return a + b;
+    int suma(int a, int b) {
+        return a + b;
+    }
+
+    int resta(int a, int b) {
+        return a - b;
+    }
 }
 
-int resta(int a, int b) {
-    return a - b;
+int main() {
+    int num1, num2;
+    std::string text;
+
+    std::cout << "Introduce el primer numero: ";
+    std::cin >> num1;
+
+    std::cout << "Introduce el segundo numero: ";
+    std::cin >> num2;
+
+    std::cout << "La suma de los numeros es: " << Matematicas::suma(num1, num2) << std::endl;
+    std::cout << "La resta de los numeros es: " << Matematicas::resta(num1, num2) << std::endl;
+
+    std::cout << "Ingresa un texto: ";
+    std::cin >> text;
+    std::cout << "El número ingresado es: " << num1 << "\nEl texto ingresado es: " << text << std::endl;
+
+    return 0;
 }
-
-
